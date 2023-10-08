c832b06c98a10d84737741280a9f1639cf54f651
MaxVzl <maxime.vozelle@gmail.com>
2023-10-08 21:54:48 +0200

    comment testcontroller

--- Base-API/Controllers/TestController.cs
+++ Base-API/Controllers/TestController.cs
@@ -15,46 +15,46 @@ public class TestController : ControllerBase
         _context = context;
     }
     
-    public async void Test()
-    {
-        // créer un client
-        var customer = new Customer
-        {
-            Name = "John Doe",
-            Email = "john.doe@gmail.com",
-            Password = "1234",
-            Address = "1 rue de la Paix",
-            Phone = "0654897423"
-        };
-        
-        // créer un fournisseur
-        var supplier = new Supplier
-        {
-            Name = "John Doe",
-            Email = "john.doe@gmail.com",
-            Password = "1234",
-            CompanyName = "John Doe Company",
-        };
-        
-        // créer un item pour le fournisseur
-        var item = new Item
-        {
-            Name = "Item 1",
-            Supplier = supplier,
-        };
-        
-        // créer une commande pour le client
-        var order = new Order
-        {
-            Customer = customer,
-        };
-        
-        // créer un itemOrder pour la commande
-        var orderItem = new OrderItem
-        {
-            Item = item,
-            Order = order,
-            Quantity = 1,
-        };
-    }
+    // public async void Test()
+    // {
+    //     // créer un client
+    //     var customer = new Customer
+    //     {
+    //         Name = "John Doe",
+    //         Email = "john.doe@gmail.com",
+    //         Password = "1234",
+    //         Address = "1 rue de la Paix",
+    //         Phone = "0654897423"
+    //     };
+    //     
+    //     // créer un fournisseur
+    //     var supplier = new Supplier
+    //     {
+    //         Name = "John Doe",
+    //         Email = "john.doe@gmail.com",
+    //         Password = "1234",
+    //         CompanyName = "John Doe Company",
+    //     };
+    //     
+    //     // créer un item pour le fournisseur
+    //     var item = new Item
+    //     {
+    //         Name = "Item 1",
+    //         Supplier = supplier,
+    //     };
+    //     
+    //     // créer une commande pour le client
+    //     var order = new Order
+    //     {
+    //         Customer = customer,
+    //     };
+    //     
+    //     // créer un itemOrder pour la commande
+    //     var orderItem = new OrderItem
+    //     {
+    //         Item = item,
+    //         Order = order,
+    //         Quantity = 1,
+    //     };
+    // }
 }
